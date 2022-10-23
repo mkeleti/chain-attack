183099d62753c22e2bd70b529fa50b152d6b2e7a
mkeleti <mmkeleti@gmail.com>
2022-10-23 14:36:18 -0500

    hkhjhkjhkj

---
+++ blockchain/monitored-node/loadscript.js
@@ -0,0 +1,11 @@
+/*
+// personal.unlockAccount(eth.Accounts[0], "");
+//miner.start();
+function Simulate() {
+  from = Range(1, 10);
+  to = Range(1, 10);
+  personal.unlockAccount(eth.accounts[from], "");
+  eth.sendTransaction({from: eth.accounts[from], to: eth.accounts[to], value: web3.toWei(1, "ether")});
+}
+
+//setInterval(Simulate(), 1000);
