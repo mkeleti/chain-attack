fe47de7f5855f639aabd849bb4fbce22a2c6a8c0
Michael Keleti <mmkeleti@gmail.com>
2022-11-18 20:29:41 -0600

    Website No longer container

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+trap_ctrlc() {
+    yarn untransact
+    docker compose down
+    echo "Goodbye!"
+    kill -9 $(lsof -t -i:"3000")
+    wait $(lsof -t -i:"3000")
+    echo -e "\n\ntrap_ctrlc\n\n"
+}
+
 if [ -x "$(command -v docker)" ]; then
     echo "Updating git repo"
     git pull
@@ -8,8 +18,6 @@ if [ -x "$(command -v docker)" ]; then
     docker compose build
     echo "Starting Docker Containers"
     docker compose up -d
-    echo "Opening website"
-    python -m webbrowser -n "http://127.0.0.1:3000"
     echo "Pausing & Unpausing miners for correct sync"
     sleep 10
     docker compose restart geth-signer-2
@@ -17,12 +25,13 @@ if [ -x "$(command -v docker)" ]; then
     # ./injectRpcArgs.sh # 51 Attack
     cd Dashboard || exit
     yarn global add pm2
+    echo "Make sure to install pm2 with your package manager if you get errors after this message."
     yarn install
     yarn transact
-    read -p "Press Enter to continue" </dev/tty
-    yarn untransact
-    docker compose down
-    echo "Goodbye!"
+    yarn build
+    echo "Opening website, press CTRL+C to stop"
+    trap trap_ctrlc INT
+    yarn start
 else
     echo "Install docker to run: https://www.docker.com"
 fi
