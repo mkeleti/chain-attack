547f2d9d6a9f7bb89e59f9a855f65e56689cce45
Emma Nasseri <enasseri03@gmail.com>
2022-11-22 07:08:25 -0600

    Create Nodes
    Nodes get created into an array

--- Dashboard/components/simulation/Blockchain.tsx
+++ Dashboard/components/simulation/Blockchain.tsx
@@ -2,14 +2,27 @@ import { Box } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { Subscription } from "../../web3/newHeads";
 import Block from "./elements/Block";
+import Node from "./elements/Node";
 
 interface PropTypes {
   hashes: string[];
   nodes: string[];
 }
+
 export const Blockchain = ({ nodes }: PropTypes) => {
   const [blocks, setBlocks] = useState([]);
   const [index, setIndex] = useState(0);
+  const nodeArray = [];
+  const nodeCount = nodes.length;
+
+  if(index == 0){         // this is supposed to initialize nodes but it actually doesnt work
+    for (let i = 0; i < nodeCount; i++) {   
+      nodeArray.push(Node(i));
+    }
+    console.log("there are ", nodeArray.length, " nodes");
+  }  
+
+  
 
   useEffect(() => {
     Subscription.on("data", (data) => {
@@ -37,5 +50,10 @@ export const Blockchain = ({ nodes }: PropTypes) => {
     return () => {};
   });
 
-  return <Box>{blocks}</Box>;
+  return (
+    <div>
+      <Box>{nodeArray}</Box>
+      <Box>{blocks}</Box>;
+    </div>
+  )
 };
