3b375ab09a3ffff1cc146f5a17e1f95505764bc5
Alexis Vielma <alexis.vielma.us@gmail.com>
2022-11-21 22:14:46 -0600

    refactor(web): seperate header component

--- Dashboard/components/layout/BaseLayout.tsx
+++ Dashboard/components/layout/BaseLayout.tsx
@@ -1,40 +1,14 @@
-import { AppShell, Button, Menu } from "@mantine/core";
-import Link from "next/link";
-import { FC, ReactNode } from "react";
+import { AppShell } from "@mantine/core";
+import type { FC, ReactNode } from "react";
+import { Header } from "./navigation/Header";
 
 interface Props {
   children: ReactNode;
 }
+
 export const BaseLayout: FC<Props> = ({ children }) => {
   return (
-    <AppShell
-      header={
-        <Menu
-          shadow="md"
-          width={200}
-          transition="rotate-right"
-          transitionDuration={150}
-        >
-          <Menu.Target>
-            <Button sx={{ margin: 15 }}>Menu</Button>
-          </Menu.Target>
-
-          <Menu.Dropdown>
-            <Menu.Label>Application</Menu.Label>
-            <Link href="/">
-              <Menu.Item>Visualizer</Menu.Item>
-            </Link>
-            <Link href="/blockscout">
-              <Menu.Item>Blockscout</Menu.Item>
-            </Link>
-            <Menu.Divider />
-            <Link href="/attack">
-              <Menu.Item color="red">Attack</Menu.Item>
-            </Link>
-          </Menu.Dropdown>
-        </Menu>
-      }
-    >
+    <AppShell header={<Header />}>
       <main>{children}</main>
     </AppShell>
   );
--- Dashboard/components/layout/navigation/Header.tsx
+++ Dashboard/components/layout/navigation/Header.tsx
@@ -1,93 +1,31 @@
-import { Burger, Container, createStyles, Group, Header } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import NextButton from "./NextButton";
+import { Button, Menu } from "@mantine/core";
+import Link from "next/link";
 
-const useStyles = createStyles((theme) => ({
-  header: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "100%",
-  },
-
-  links: {
-    [theme.fn.smallerThan("xs")]: {
-      display: "none",
-    },
-  },
-
-  burger: {
-    [theme.fn.largerThan("xs")]: {
-      display: "none",
-    },
-  },
-
-  link: {
-    display: "block",
-    lineHeight: 1,
-    padding: "8px 12px",
-    borderRadius: theme.radius.sm,
-    textDecoration: "none",
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-  },
-
-  linkActive: {
-    "&, &:hover": {
-      backgroundColor: theme.fn.variant({
-        variant: "light",
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({
-        variant: "light",
-        color: theme.primaryColor,
-      }).color,
-    },
-  },
-}));
-
-interface HeaderSimpleProps {
-  links: { link: string; label: string }[];
-}
-
-export function HeaderSimple({ links }: HeaderSimpleProps) {
-  const [opened, { toggle }] = useDisclosure(false);
-  const { classes } = useStyles();
-
-  const items = links.map((link) => (
-    <NextButton
-      color="blue"
-      key={`${link.label}-`}
-      href={link.link}
-      title={link.label}
-    />
-  ));
-
+export const Header = () => {
   return (
-    <Header height={60} mb={120}>
-      <Container className={classes.header}>
-        KU Blockchain Explorer
-        <Group spacing={5} className={classes.links}>
-          {items}
-        </Group>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          className={classes.burger}
-          size="sm"
-        />
-      </Container>
-    </Header>
+    <Menu
+      shadow="md"
+      width={200}
+      transition="rotate-right"
+      transitionDuration={150}
+    >
+      <Menu.Target>
+        <Button sx={{ margin: 15 }}>Menu</Button>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Label>Application</Menu.Label>
+        <Link href="/">
+          <Menu.Item>Visualizer</Menu.Item>
+        </Link>
+        <Link href="/blockscout">
+          <Menu.Item>Blockscout</Menu.Item>
+        </Link>
+        <Menu.Divider />
+        <Link href="/attack">
+          <Menu.Item color="red">Attack</Menu.Item>
+        </Link>
+      </Menu.Dropdown>
+    </Menu>
   );
-}
+};
