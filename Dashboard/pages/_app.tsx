adb3b3d4e721676d17317e62ecc71dd0dc1a5c06
Alexis Vielma <alexis.vielma.us@gmail.com>
2022-11-21 22:37:35 -0600

    style(web): remove commented code

--- Dashboard/pages/_app.tsx
+++ Dashboard/pages/_app.tsx
@@ -2,22 +2,12 @@
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 import { AppProps } from "next/app";
-import Head from "next/head";
 import { BaseLayout } from "../components/";
-
-// Simulate();
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   return (
     <div>
-      <Head>
-        <title>Page title</title>
-        {/* <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        /> */}
-      </Head>
       {/* Add Mantine Provider with provides global themes. */}
       <MantineProvider withGlobalStyles withNormalizeCSS>
         <NotificationsProvider position="top-right" zIndex={2077}>
