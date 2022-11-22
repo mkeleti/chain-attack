b55ff2456c5199581d2bd1d1564801e4bb9acd1f
Alexis Vielma <alexis.vielma.us@gmail.com>
2022-11-21 21:40:23 -0600

    feat(web): relocate layout components

---
+++ Dashboard/components/layout/BaseLayout.tsx
@@ -0,0 +1,42 @@
+import { AppShell, Button, Menu } from "@mantine/core";
+import Link from "next/link";
+import { FC, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+export const BaseLayout: FC<Props> = ({ children }) => {
+  return (
+    <AppShell
+      header={
+        <Menu
+          shadow="md"
+          width={200}
+          transition="rotate-right"
+          transitionDuration={150}
+        >
+          <Menu.Target>
+            <Button sx={{ margin: 15 }}>Menu</Button>
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            <Menu.Label>Application</Menu.Label>
+            <Link href="/">
+              <Menu.Item>Visualizer</Menu.Item>
+            </Link>
+            <Link href="/blockscout">
+              <Menu.Item>Blockscout</Menu.Item>
+            </Link>
+            <Menu.Divider />
+            <Menu.Item color="red">Attack</Menu.Item>
+            <Link href="/attack">
+              <Menu.Item>Attack</Menu.Item>
+            </Link>
+          </Menu.Dropdown>
+        </Menu>
+      }
+    >
+      <main>{children}</main>
+    </AppShell>
+  );
+};
---
+++ Dashboard/components/layout/navigation/NextButton.tsx
@@ -0,0 +1,22 @@
+import { Button, DefaultMantineColor, MantineSize } from "@mantine/core";
+import Link from "next/link";
+import { UrlObject } from "url";
+
+declare type Url = string | UrlObject;
+
+type ButtonProps = {
+  href: Url;
+  color?: DefaultMantineColor | undefined;
+  size?: MantineSize | undefined;
+  title: string;
+};
+
+const NextButton = (props: ButtonProps) => (
+  <Link href={props.href} passHref>
+    <Button component="a" color={props.color} size={props.size}>
+      {props.title}
+    </Button>
+  </Link>
+);
+
+export default NextButton;
