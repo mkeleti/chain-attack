7c8004afb3440c2d30ba8039248ab2b73d0a98ce
Michael Keleti <mmkeleti@gmail.com>
2022-11-29 00:48:17 -0600

    Add Prettier & Eslint

---
+++ Dashboard/app/dockerode.ts
@@ -0,0 +1,25 @@
+import Dockerode from 'dockerode';
+import { NextPage } from 'next';
+
+export const dsckt = new Dockerode();
+
+export async function nodeNumber(): Promise<{nodes: number}> {
+  const allContainers = await dsckt.listContainers();
+  let node_number: number= allContainers.length;
+  for (const container of allContainers) {
+    if (!container.Image.includes('geth')) {
+      node_number--;
+    }
+  }
+  return {nodes: node_number};
+}
+
+export default function dockerode() {
+  let x: number;
+
+  nodeNumber().then((res) => { x = res.nodes; });
+  return ( x );
+
+}
+
+
