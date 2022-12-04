c6a9a55486ffaf4ebd6f85309a484ca166b21c09
Alexis Vielma <alexis.vielma.us@gmail.com>
2022-12-04 17:58:44 -0600

    refactor(dashboard): remove redundant components

--- Dashboard/pages/_app.tsx
+++ Dashboard/pages/_app.tsx
@@ -1,21 +1,14 @@
-/* eslint-disable @next/next/no-sync-scripts */
 import { MantineProvider } from "@mantine/core";
-import { NotificationsProvider } from "@mantine/notifications";
 import { AppProps } from "next/app";
 import { BaseLayout } from "../components/";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   return (
-    <div>
-      {/* Add Mantine Provider with provides global themes. */}
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <NotificationsProvider position="top-right" zIndex={2077}>
-          <BaseLayout>
-            <Component {...pageProps} />
-          </BaseLayout>
-        </NotificationsProvider>
-      </MantineProvider>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <BaseLayout>
+        <Component {...pageProps} />
+      </BaseLayout>
+    </MantineProvider>
   );
 }
