fdd7a78826ed45e459b6510d09da17c84b2373a8
Michael Keleti <16996410+mkeleti@users.noreply.github.com>
2022-12-04 02:44:51 -0600

    refactor: Added traefik as well as prod and dev envs
    This change provides the basis for creating multiple environemnts for both dev and production

--- start.sh
+++ start.sh
@@ -7,29 +7,56 @@ trap_ctrlc() {
     wait $(lsof -t -i:"3000")
     echo -e "\n\ntrap_ctrlc\n\n"
 }
-
-if [ -x "$(command -v docker)" ]; then
-    echo "Updating git repo"
-    git pull
-    echo "Stopping any currently running containers"
-    docker compose down
-    echo "Building containers"
-    docker compose build
-    echo "Starting Docker Containers"
-    docker compose up -d
-    echo "Pausing & Unpausing miners for correct sync"
-    sleep 10
-    docker compose restart geth-signer-2
-    docker compose restart geth-signer-3
-    echo "Done! You can access the website at http://127.0.0.1:3000"
-    # ./injectRpcArgs.sh # 51 Attack
-    cd Dashboard || exit
-    yarn install
-    # yarn build
-    echo "Opening website, press CTRL+C to stop"
-    trap trap_ctrlc INT
-    # yarn start
-    yarn dev
+if [ "$1" = "prod" ]; then
+    if [ -x "$(command -v docker)" ]; then
+        echo "Updating git repo: production"
+        git pull
+        echo "Stopping any currently running containers"
+        docker compose down
+        echo "Building containers"
+        docker compose build
+        echo "Starting Docker Containers"
+        docker compose up -d
+        echo "Pausing & Unpausing miners for correct sync"
+        sleep 10
+        docker compose restart geth-signer-2
+        docker compose restart geth-signer-3
+        echo "Done! You can access the website at http://127.0.0.1:3000"
+        # ./injectRpcArgs.sh # 51 Attack
+        cd Dashboard || exit
+        yarn install
+        # yarn build
+        echo "Opening website, press CTRL+C to stop"
+        trap trap_ctrlc INT
+        # yarn start
+        yarn dev
+    else
+        echo "Install docker to run: https://www.docker.com"
+    fi
 else
-    echo "Install docker to run: https://www.docker.com"
+    if [ -x "$(command -v docker)" ]; then
+        echo "Updating git repo"
+        git pull
+        echo "Stopping any currently running containers"
+        docker compose down
+        echo "Building containers"
+        docker compose build
+        echo "Starting Docker Containers"
+        docker compose up -d
+        echo "Pausing & Unpausing miners for correct sync"
+        sleep 10
+        docker compose restart geth-signer-2
+        docker compose restart geth-signer-3
+        echo "Done! You can access the website at http://127.0.0.1:3000"
+        # ./injectRpcArgs.sh # 51 Attack
+        cd Dashboard || exit
+        yarn install
+        # yarn build
+        echo "Opening website, press CTRL+C to stop"
+        trap trap_ctrlc INT
+        # yarn start
+        yarn dev
+    else
+        echo "Install docker to run: https://www.docker.com"
+    fi
 fi
