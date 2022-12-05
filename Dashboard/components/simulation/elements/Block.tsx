e0bf2f1ca3bfd2b93f39ef47488fe1e3c91a6754
Alexis Vielma <alexis.vielma.us@gmail.com>
2022-12-04 18:01:41 -0600

    feat(dashboard): animate block

--- Dashboard/components/simulation/elements/Block.tsx
+++ Dashboard/components/simulation/elements/Block.tsx
@@ -1,52 +1,83 @@
 import { Box, Group, HoverCard, Text } from "@mantine/core";
+import { motion } from "framer-motion";
 import type { FC } from "react";
-import { BlockHeader } from "web3-eth";
 
-interface propTypes {
-  data: BlockHeader;
-}
+type Props = {
+  block: any;
+  index: number;
+};
 
-const Block: FC<any> = (props: propTypes) => {
-  console.log(props.data);
+export const Block: FC<Props> = ({ block, index }) => {
   return (
-    <HoverCard>
-      <HoverCard.Target>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: 110,
-            height: 85,
-            borderRadius: 8,
-            borderWidth: 1,
-            borderColor: "#dee2e6",
-            borderStyle: "solid",
-            backgroundColor: "#fff",
-            boxShadow:
-              "0 1px 3px rgba(0, 0, 0, 0.05),rgba(0, 0, 0, 0.05) 0px 20px 25px -5px,rgba(0, 0, 0, 0.04) 0px 10px 10px -5px",
-          }}
-        >
-          {props.data.number}
-        </Box>
-      </HoverCard.Target>
-      <HoverCard.Dropdown>
-        <Group>
-          <Text size="sm" weight="bold">
-            Block #{props.data.number}
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {index > 0 && (
+        <motion.svg id={`line-${index}`} width="110" height="55">
+          <motion.line
+            x1="55"
+            y1="0"
+            x2="55"
+            y2="55"
+            stroke="#dee2e6"
+            strokeWidth="1"
+            initial={{
+              pathLength: 0,
+            }}
+            animate={{
+              pathLength: 1,
+            }}
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+            }}
+          ></motion.line>
+        </motion.svg>
+      )}
+      <HoverCard>
+        <HoverCard.Target>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.2 }}
+            drag
+            whileDrag={{ scale: 1.2 }}
+            dragSnapToOrigin
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: 110,
+                height: 85,
+                borderRadius: 8,
+                borderWidth: 1,
+                borderColor: "#dee2e6",
+                borderStyle: "solid",
+                backgroundColor: "#fff",
+                cursor: "pointer",
+              }}
+            >
+              <Text size="sm" weight="bold">
+                {block.number}
+              </Text>
+            </Box>
+          </motion.div>
+        </HoverCard.Target>
+        <HoverCard.Dropdown>
+          <Group>
+            <Text size="sm" weight="bold">
+              Blockscout
+            </Text>
+          </Group>
+          <Text size="xs" mt="md" color="">
+            <b>Hash:</b> {block.hash}
           </Text>
-        </Group>
-        <Text size="xs" mt="md" color="">
-          <b>Hash:</b> {props.data.hash}
-        </Text>
-        <Text size="xs" mt="md" color="">
-          <b>Time:</b> {formatTime(props.data.timestamp)}
-        </Text>
-        <Text size="xs" mt="md" color="">
-          <b>Gas Used:</b> {props.data.gasUsed}
-        </Text>
-      </HoverCard.Dropdown>
-    </HoverCard>
+          <Text size="xs" mt="md" color="">
+            <b>Time:</b> {formatTime(block.timestamp)}
+          </Text>
+        </HoverCard.Dropdown>
+      </HoverCard>
+    </div>
   );
 };
 
