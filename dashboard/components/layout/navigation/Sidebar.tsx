d6b137351a313c95a4aa5da3e26b00aadc3c8c60
Alexis Vielma <alexis.vielma.us@gmail.com>
2022-12-05 21:18:54 -0600

    feat: :construction: add miner logs to sidebar

--- dashboard/components/layout/navigation/Sidebar.tsx
+++ dashboard/components/layout/navigation/Sidebar.tsx
@@ -1,21 +1,45 @@
-import { Aside, Button, ScrollArea, Stack } from "@mantine/core";
+import { Aside, Button, ScrollArea, Stack, Divider, Tabs } from "@mantine/core";
 
 import Link from "next/link";
+import { MinerLog } from "../MinerLog";
 
 export const Sidebar = () => {
   return (
     <ScrollArea style={{ height: 250 }}>
-      <Aside p="md" width={{ lg: 300 }}>
+      <Aside p="md" width={{ lg: 405 }}>
         <Stack>
-          <Link href="">
-            <Button>Blockchain Explorer</Button>
+          <Link href="/blockscout">
+            <Button fullWidth variant="light" color="blue">
+              Blockchain Explorer
+            </Button>
           </Link>
-          <Link href="">
-            <Button>Start Attack</Button>
+          <Link href="/attack">
+            <Button fullWidth variant="light" color="red">
+              Start Attack
+            </Button>
           </Link>
-          <Link href="">
-            <Button>View the Chain</Button>
+          <Link href="/">
+            <Button fullWidth variant="light" color="teal">
+              View the Chain
+            </Button>
           </Link>
+          <Divider my="xxs" />
+          <Tabs defaultValue="miner01">
+            <Tabs.List grow>
+              <Tabs.Tab value="miner01">Miner 1</Tabs.Tab>
+              <Tabs.Tab value="miner02">Miner 2</Tabs.Tab>
+              <Tabs.Tab value="miner03">Miner 3</Tabs.Tab>
+            </Tabs.List>
+            <Tabs.Panel value="miner01" pt="xs">
+              <MinerLog source="http://localhost/logs/0" />
+            </Tabs.Panel>
+            <Tabs.Panel value="miner02" pt="xs">
+              <MinerLog source="http://localhost/logs/1" />
+            </Tabs.Panel>
+            <Tabs.Panel value="miner03" pt="xs">
+              <MinerLog source="http://localhost/logs/2" />
+            </Tabs.Panel>
+          </Tabs>
         </Stack>
       </Aside>
     </ScrollArea>
